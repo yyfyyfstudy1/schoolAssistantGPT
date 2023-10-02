b982b60308daa80830fb95731b014a47ec23357a
Helena Cui <ziyuan.cui@sydney.edu.au>
2023-10-02 14:57:01 +1100

    Update profile fragment visual

--- app/src/main/java/comp5216/sydney/edu/au/learn/fragment/profileFragment.java
+++ app/src/main/java/comp5216/sydney/edu/au/learn/fragment/profileFragment.java
@@ -4,14 +4,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 import comp5216.sydney.edu.au.learn.R;
 
 public class profileFragment extends Fragment {
+
+    private TextView nameTextView;
+    private TextView emailTextView;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -22,6 +30,20 @@ public class profileFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        nameTextView = view.findViewById(R.id.profileName);
+        emailTextView = view.findViewById(R.id.profileEmail);
+
+        if(user != null) {
+            String userName = user.getDisplayName();
+            String userEmail = user.getEmail();
+
+            if(userName != null)
+                nameTextView.setText(userName);
+            if(userEmail != null)
+                emailTextView.setText(userEmail);
+        }
 
     }
 }
